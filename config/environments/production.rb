8d603fa5b8cbf87d637affd5c6e75fde82a3d455
Michael Sipayung <Michaelsipayung123@gmail.com>
2024-01-10 11:15:05 +0700

    mailgun configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,19 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'https://radiant-ridge-52163-0408a552c9ca.herokuapp.com/'
+  config.action_mailer.default_url_options = {host: host}
+  ActionMailer::Base.smtp_settings = {
+    :port           => 587,
+    :address        => 'smtp.mailgun.org',
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'heroku.com',
+    :authentication => :plain,
+  }
+  #ActionMailer::Base.delivery_method = :smtp
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
