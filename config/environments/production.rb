884bc4440bb3541eabba5fc7e64bb48303680fa8
Michael Sipayung <Michaelsipayung123@gmail.com>
2024-01-10 12:24:44 +0700

    production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,20 +75,15 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
-  host = 'https://radiant-ridge-52163-0408a552c9ca.herokuapp.com/'
-  config.action_mailer.default_url_options = {host: host}
-  ActionMailer::Base.smtp_settings = {
-    :port           => 587,
-    :address        => 'smtp.mailgun.org',
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'heroku.com',
-    :authentication => :plain,
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp-relay.sendinblue.com',
+    :port => 587,
+    :user_name => ENV['SENDINBLUE_SMTP_LOGIN'],
+    :password => ENV['SENDINBLUE_SMTP_PASSWORD'],
+    :authentication => :login
   }
-  #ActionMailer::Base.delivery_method = :smtp
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
