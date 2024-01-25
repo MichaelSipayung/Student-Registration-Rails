865bc1c426509c54f69058a8718f56aa8cf1e4af
Michael Sipayung <Michaelsipayung123@gmail.com>
2024-01-25 15:28:12 +0700

    utbk score model, bug fix

--- app/controllers/utbk_scores_controller.rb
+++ app/controllers/utbk_scores_controller.rb
@@ -1,16 +1,40 @@
 class UtbkScoresController < ApplicationController
   def new
+    @utbk_score = current_user.build_utbk_score
   end
 
   def update
+    @utbk_score = current_user.utbk_score
+    if @utbk_score.update(utbk_score_params)
+      flash[:success] = " utbk score is updated"
+    else
+      render 'edit'
+    end
   end
 
   def create
+    @utbk_score = current_user.build_utbk_score(utbk_score_params)
+    if @utbk_score.save
+      flash[:success] = "utbk score is saved"
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @utbk_score = current_user.utbk_score
   end
 
   def show
+  end
+  private
+  def utbk_score_params
+    params.require(:utbk_score).permit(:no_peserta, :tanggal_ujian,
+                                       :jumlah_nilai_semester_6,
+                                       :jumlah_pelajaran_semester_6,
+                                       :nilai_penalaran_umum,
+                                       :nilai_pengetahuan_kuantitatif,
+                                       :nilai_kemampuan_memahami_bacaan_dan_menulis,
+                                       :nilai_pengetahuan_dan_pemahaman_umum)
   end
 end
--- test/controllers/utbk_scores_controller_test.rb
+++ test/controllers/utbk_scores_controller_test.rb
@@ -1,28 +1,36 @@
 require "test_helper"
 
 class UtbkScoresControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    get login_path
+    post login_path, params: {session: {
+      email: users(:michael).email, password: 'password'
+    }}
+  end
   test "should get new" do
-    get utbk_scores_new_url
+    assert is_logged_in?
+    get new_utbk_score_url
     assert_response :success
   end
 
-  test "should get update" do
-    get utbk_scores_update_url
-    assert_response :success
-  end
-
-  test "should get create" do
-    get utbk_scores_create_url
-    assert_response :success
-  end
-
+  # test "should get update" do
+  #   get utbk_scores_update_url
+  #   assert_response :success
+  # end
+  #
+  # test "should get create" do
+  #   get utbk_scores_create_url
+  #   assert_response :success
+  # end
+  #
   test "should get edit" do
-    get utbk_scores_edit_url
+    assert is_logged_in?
+    get edit_utbk_score_url(utbk_scores(:utbk_score_two))
     assert_response :success
   end
-
-  test "should get show" do
-    get utbk_scores_show_url
-    assert_response :success
-  end
+  #
+  # test "should get show" do
+  #   get utbk_scores_show_url
+  #   assert_response :success
+  # end
 end
