bb520822afa1429e13f4c524de70dfb590f6a11c
Michael Sipayung <Michaelsipayung123@gmail.com>
2024-02-01 12:09:33 +0700

    experimental api for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,9 @@
 
 # handle sessions (login, logout, etc.) for the application
 class SessionsController < ApplicationController
+  # skip_before_action :verify_authenticity_token, only: [:create]
   skip_before_action :logged_in_user, only: %i[new create] # skip the logged_in_user method for new and create action
+
   def new; end
 
   def create
@@ -22,6 +24,33 @@ class SessionsController < ApplicationController
       render 'new'
     end
   end
+
+  # def create
+  #   user = User.find_by(email: params[:session][:email].downcase)
+  #   if user&.authenticate(params[:session][:password])
+  #     if user.activated
+  #       log_in user
+  #       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+  #       respond_to do |format|
+  #         format.html { redirect_back_or user }
+  #         format.json { render json: { success: true, user: user }, status: :ok }
+  #       end
+  #     else
+  #       message = 'Account not activated'
+  #       flash[:warning] = message
+  #       respond_to do |format|
+  #         format.html { redirect_to root_url }
+  #         format.json { render json: { success: false, message: message }, status: :unauthorized }
+  #       end
+  #     end
+  #   else
+  #     flash.now[:danger] = 'Invalid email/password combination'
+  #     respond_to do |format|
+  #       format.html { render 'new' }
+  #       format.json { render json: { success: false, message: 'Invalid email/password combination' }, status: :unauthorized }
+  #     end
+  #   end
+  # end
 
   # for case : we open two tab in browser, in one window we already logout
   def destroy
